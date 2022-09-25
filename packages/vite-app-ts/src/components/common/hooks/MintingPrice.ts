18ae7a2509938030fdc9192de7bdb7d973c06708
lesquive77 <les.quive77@gmail.com>
2022-09-25 09:04:07 +0200

    fix minting price hook

--- packages/vite-app-ts/src/components/common/hooks/MintingPrice.ts
+++ packages/vite-app-ts/src/components/common/hooks/MintingPrice.ts
@@ -1,6 +1,7 @@
 import { useContractReader } from 'eth-hooks';
 import { useEthersContext } from 'eth-hooks/context';
 import { BigNumber, utils } from 'ethers';
+import { useEffect, useState } from 'react';
 
 import { UserEntry } from '~~/components/pages/nft/NFTUI';
 import { useAppContracts } from '~~/config/contractContext';
@@ -14,25 +15,29 @@ export const useMintingPrice = (discountEntry: UserEntry | undefined): BigNumber
     discountEntry?.address as string,
   ])[0];
   const duration = useContractReader(mysteryBoxToken, mysteryBoxToken?.duration)[0] as BigNumber;
+  const [bnPrice, setBnPrice] = useState<BigNumber>();
 
-  let price = 0.2;
-  if (startingTime && '0' === startingTime.toString()) {
-    console.log(discountEntry?.discountRate);
-    if (discountEntry && !claimed) {
-      price *= 0.01 * (100 - discountEntry.discountRate);
+  useEffect(() => {
+    let price = 0.2;
+    if (startingTime && '0' === startingTime.toString() && discountEntry) {
+      console.log(discountEntry?.discountRate);
+      if (discountEntry && !claimed) {
+        price *= 0.01 * (100 - discountEntry.discountRate);
+      }
+      setBnPrice(utils.parseEther(price.toString()));
     }
-    return utils.parseEther(price.toString());
-  }
-  if (duration && startingTime) {
-    console.log(discountEntry?.discountRate);
-    const percentage = (Date.now() / 1000 - startingTime.toNumber()) / duration.toNumber();
-    console.log(percentage);
-    price += 0.3 * percentage;
-    if (discountEntry && !claimed) {
-      price *= 0.01 * (100 - discountEntry.discountRate);
+    if (duration && startingTime && discountEntry) {
+      console.log(discountEntry?.discountRate);
+      const percentage = (Date.now() / 1000 - startingTime.toNumber()) / duration.toNumber();
+      console.log(percentage);
+      price += 0.3 * percentage;
+      if (discountEntry && !claimed) {
+        price *= 0.01 * (100 - discountEntry.discountRate);
+      }
+      console.log(price);
+      setBnPrice(utils.parseEther(price.toString().substring(0, 19)));
     }
-    console.log(price);
-    return utils.parseEther(price.toString().substring(0, 19));
-  }
-  return undefined;
+  }, [claimed, duration, discountEntry, startingTime, mysteryBoxToken]);
+
+  return bnPrice;
 };
